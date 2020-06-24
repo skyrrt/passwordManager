1732cf53a29957470a7c5c581b52ae17917f0735
Bartek <bart.rzyszkiewicz@gmail.com>
2020-06-24 17:39:54 +0200

    init

---
+++ PasswordManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordManager
+//
+//  Created by Bartek Rzyszkiewicz on 24/06/2020.
+//  Copyright © 2020 Bartek Rzyszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
