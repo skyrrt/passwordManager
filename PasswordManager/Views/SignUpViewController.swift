a6957fddd395b68762a47dc57b28a210757c1450
Bartek <bart.rzyszkiewicz@gmail.com>
2020-07-09 23:29:46 +0200

    signupcontroller outlets

--- PasswordManager/Views/SignUpViewController.swift
+++ PasswordManager/Views/SignUpViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class SignUpViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
     }
 }
